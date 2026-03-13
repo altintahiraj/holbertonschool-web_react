2c7f6657468be4d3e0fd9510caebcbcbd58bc3ea
altintahiraj <altintahiraj357@gmail.com>
2026-03-13 21:52:16 +0100

    task-0 fixed

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import holbertonLogo from './assets/holberton-logo.jpg'
-import myDate from './utils'
+import logo from './assets/holberton-logo.jpg';
 
-function App() {
+
+import './App.css'
+
+const App = () => {
+  const date = new Date()
+
 
   return (
     <>
-      <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo"></img>
-        <h1 style={{color: "#e1003c"}}>School Dashboard</h1>
+
+      <div className='App-header'>
+        <img src={logo} alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+      <div className='App-body'>
+
+        <p >Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">
-        <p>Copyright {myDate.getCurrentYear()} - {myDate.getFooterCopy(true)}</p>
+
+      <div className='App-footer'>
+
+        <p >Copyright {date.getFullYear()} - holberton School</p>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,14 +1,19 @@
 import { render, screen } from '@testing-library/react';
-import App from "./App";
+import App from './App';
 
-test("renders 'School Dashboard' h1 element", () => {
+test('renders h1 with text School Dashboard', () => {
     render(<App />);
-    const titleElement = screen.getByRole("heading", { level: 1, name: /school dashboard/i });
-    expect(titleElement).toBeInTheDocument();
-    const bodyText = screen.getByText(/login to access the full dashboard/i);
-    expect(bodyText).toBeInTheDocument();
-    const footerText = screen.getByText(/copyright/i);
-    expect(footerText).toBeInTheDocument();
-    const imgElement = screen.getByRole("img");
-    expect(imgElement).toBeInTheDocument();
+    expect(screen.getByText(/School Dashboard/i)).toBeInTheDocument();
+});
+
+test('renders correct text in app-body and app-footer', () => {
+    render(<App />);
+    expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/Copyright/i)).toBeInTheDocument();
+});
+
+test('renders an img element', () => {
+    render(<App />);
+    const img = screen.getByAltText(/holberton logo/i);
+    expect(img).toBeInTheDocument();
 });
