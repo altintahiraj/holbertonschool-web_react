ce0c7a5b0767784e7e26fbac5452db80a7ea7c4a
altintahiraj <altintahiraj357@gmail.com>
2026-03-16 20:09:08 +0100

    update

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -16,20 +16,22 @@ function Notifications() {
         }}
         onClick={() => console.log("Close button has been clicked")}
       >
-        <img src={closeIcon} alt="Close icon" width={10} />
+        <img src={closeIcon} alt="Close icon" width="10px" />
       </button>
 
       <p>Here is the list of notifications</p>
 
       <ul>
         <li data-priority="default">New course available</li>
+
         <li data-priority="urgent">New resume available</li>
+
         <li
           data-priority="urgent"
           dangerouslySetInnerHTML={{
             __html: getLatestNotification(),
           }}
-        />
+        ></li>
       </ul>
     </div>
   );
